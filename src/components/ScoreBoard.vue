52e898daad2af7b7219c57b7d55dbf9cb3848791
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-12-04 22:01:03 +0100

    feat: receive websocket message

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+
+let socket= null;
+let data= ref([{
+    team: "Tfue",
+    score: "5",
+}, {
+    team: "AsianJeff",
+    score: "3",
+}]);
+
+onMounted(() => {
+  // Connect to WebSocket server
+  socket = new WebSocket('ws://localhost:3000/primus'); // Use 'ws' instead of 'http'
+
+  socket.onmessage= (event)=>{
+        // console.log(event.data);
+        let newScore= JSON.parse(event.data);
+        console.log(newScore);
+        if(newScore.data.action==="updateStats"){
+            data.value.push({
+                team: newScore.data.team,
+                score: newScore.data.score,
+            });
+        }
+    }
+});
+</script>
+
+<template>
+  <div>
+    <ul>
+        <li v-for="item in data">
+            {{ item.team }}: {{ item.score }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
